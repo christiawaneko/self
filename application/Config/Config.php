06f78c511cb8a02b20e2e78cba612bff4bcf6de2
christiawaneko <christiawaneko@gmail.com>
2018-04-26 12:05:07 +0700

    Foundation

--- application/Config/Config.php
+++ application/Config/Config.php
@@ -15,7 +15,7 @@ define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'kerjasama');
+define('DB_NAME', 'YourDB');
 define('DB_USER', 'root');
-define('DB_PASS', 'tamvan');
+define('DB_PASS', 'root');
 define('DB_CHARSET', 'utf8');
